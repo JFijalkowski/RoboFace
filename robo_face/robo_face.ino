c43d1e706db26bb54af5bec70f39621cbbf2e847
Jfijalkowski <jonathanfijallkowski@gmail.com>
2025-10-11 00:59:10 +0100

    add animation queue data structure, adding animation frames to queue when button press is detected

--- robo_face/robo_face.ino
+++ robo_face/robo_face.ino
@@ -12,6 +12,8 @@
 String POWER = String("BA45FF00");
 String ZERO_BUTTON = String("E916FF00");
 
+cppQueue animationQueue(4, MAX_ANIM_FRAMES, FIFO);
+
 int IR_RECEIVE_PIN = 9;
 
 //CRGB leds[NUM_LEDS];
@@ -20,26 +22,46 @@ void setup() {
   Serial.begin(9600);
   IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
   Serial.println("Started up"); 
+  int currentAnimation = ANIM_DEFAULT;
+  unsigned long animFrameStart = millis();
+  Serial.println(animFrameStart);
+  unsigned long animFrameEnd = millis() + animationData[ANIM_DEFAULT][0].millis;
+  Serial.println(animFrameEnd);
+  //start animation is default w/ blinking
+  for(int i = 0; i < MAX_ANIM_FRAMES; i++){
+    animationQueue.push(&animationData[ANIM_DEFAULT][i]);
+  }
 }
 
 void loop() {
-  if (IrReceiver.decode()) {
-      //Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX); // Print "old" raw data
-      
-      auto recvData = IrReceiver.decodedIRData.decodedRawData;
-      char hexCode [8];
-      ltoa(recvData, hexCode, 16);
-      String hexString = String(hexCode);
-      hexString.toUpperCase();
-      if(!hexString.equals("0")){
-        Serial.println(hexString);
-        int animNumber = getAnimationFromCode(codeToAnim, hexString);
-        if (animNumber != ANIM_NONE) {
-          animationFrame frame = animationData[animNumber][0];
-          Serial.println(frame.expression);
-          Serial.println(frame.millis);
+  if (IrReceiver.decode()) {    
+
+    //convert received data to 8 character hex code
+    auto recvData = IrReceiver.decodedIRData.decodedRawData;
+    char hexCode [8];
+    ltoa(recvData, hexCode, 16);
+    String hexString = String(hexCode);
+    hexString.toUpperCase();
+
+    //ignore repeated/burst data (cluster of the same code is decoded as "0")
+    if(!hexString.equals("0")){
+      Serial.println(hexString);
+      int animNumber = getAnimationFromCode(codeToAnim, hexString);
+      //a corresponding animation was found for the received code
+      if (animNumber != ANIM_NONE) {
+
+        //clear old animation from queue
+        animationQueue.flush();
+
+        //add all frames of the selected animation to the queue
+        for(int i = 0; i < MAX_ANIM_FRAMES; i++){
+          //if the frame has 0 ms, it can be ignored
+          if(animationData[animNumber][i].millis != 0){
+            animationQueue.push(&animationData[animNumber][i]);
+          }
         }
       }
-      IrReceiver.resume(); // Enable receiving of the next value
+    }
+    IrReceiver.resume(); // Enable receiving of the next value
   }
 }
